d1d3399bcd44fce9e0d323d246ce4f61f85e35cd
Unknown <sophiaarmstrong4@gmail.com>
2018-11-01 09:43:00 -0400

    Finished Conversation Class
    Added addMessage function

--- app/src/main/java/Conversation.java
+++ app/src/main/java/Conversation.java
@@ -15,125 +15,15 @@ public class Conversation {
         this.user2 = user2;
     }
 
-    List<FriendlyMessage> Message = new List<FriendlyMessage>() {
-        @Override
-        public int size() {
-            return 0;
-        }
+    List<FriendlyMessage> messageList = new LinkedList<>();
 
-        @Override
-        public boolean isEmpty() {
-            return false;
-        }
+    public List<FriendlyMessage> getMessageList(){
+        return this.messageList;
+    }
 
-        @Override
-        public boolean contains(Object o) {
-            return false;
-        }
-
-        @Override
-        public Iterator<FriendlyMessage> iterator() {
-            return null;
-        }
-
-        @Override
-        public Object[] toArray() {
-            return new Object[0];
-        }
-
-        @Override
-        public <T> T[] toArray(T[] ts) {
-            return null;
-        }
-
-        @Override
-        public boolean add(FriendlyMessage friendlyMessage) {
-            return false;
-        }
-
-        @Override
-        public boolean remove(Object o) {
-            return false;
-        }
-
-        @Override
-        public boolean containsAll(Collection<?> collection) {
-            return false;
-        }
-
-        @Override
-        public boolean addAll(Collection<? extends FriendlyMessage> collection) {
-            return false;
-        }
-
-        @Override
-        public boolean addAll(int i, @NonNull Collection<? extends FriendlyMessage> collection) {
-            return false;
-        }
-
-        @Override
-        public boolean removeAll(Collection<?> collection) {
-            return false;
-        }
-
-        @Override
-        public boolean retainAll(Collection<?> collection) {
-            return false;
-        }
-
-        @Override
-        public void clear() {
-
-        }
-
-        @Override
-        public FriendlyMessage get(int i) {
-            return null;
-        }
-
-        @Override
-        public FriendlyMessage set(int i, FriendlyMessage friendlyMessage) {
-            return null;
-        }
-
-        @Override
-        public void add(int i, FriendlyMessage friendlyMessage) {
-
-        }
-
-        @Override
-        public FriendlyMessage remove(int i) {
-            return null;
-        }
-
-        @Override
-        public int indexOf(Object o) {
-            return 0;
-        }
-
-        @Override
-        public int lastIndexOf(Object o) {
-            return 0;
-        }
-
-        @NonNull
-        @Override
-        public ListIterator<FriendlyMessage> listIterator() {
-            return null;
-        }
-
-        @NonNull
-        @Override
-        public ListIterator<FriendlyMessage> listIterator(int i) {
-            return null;
-        }
-
-        @NonNull
-        @Override
-        public List<FriendlyMessage> subList(int i, int i1) {
-            return null;
-        }
-    };
+    public void addMessage(FriendlyMessage message){
+        messageList.add(message);
+    }
 
     public boolean isMember(String username) {
         if (username.equals(user1)) {
