9e47ec8b6048a7bfa55bbe34d62334a8925c73a9
Joshua Reyes <reyesjos16@students.ecu.edu>
2018-11-24 12:39:37 -0500

    Remove unneccessary import statements in Message Activity

--- app/src/main/java/com/chat/secure/reilly/securechat/MessageActivity.java
+++ app/src/main/java/com/chat/secure/reilly/securechat/MessageActivity.java
@@ -74,9 +74,7 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
