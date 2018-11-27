4333fdd309d10ad321615e60b5aec5931eaed530
reiman2222 <reiman2222@hotmail.com>
2018-11-27 17:09:32 -0500

    removed code that destroyed main databse listner was causing crash

--- app/src/main/java/com/chat/secure/reilly/securechat/MainActivity.java
+++ app/src/main/java/com/chat/secure/reilly/securechat/MainActivity.java
@@ -249,7 +249,7 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     public void onDestroy() {
         super.onDestroy();
-        chatRef.removeEventListener(convoListener);
+        //chatRef.removeEventListener(convoListener);
     }
 
     @Override
